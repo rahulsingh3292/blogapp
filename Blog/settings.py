4a856df5055d94bb801c7ac78080ad46b4e03cb5
rahulsingh3292 <60551261+rahulsingh3292@users.noreply.github.com>
2021-10-01 21:27:41 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -132,4 +132,4 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "rahulsingh32929@gmail.com"
-EMAIL_HOST_PASSWORD = "r1a2h3u4l5"
+EMAIL_HOST_PASSWORD = "email_pass"
